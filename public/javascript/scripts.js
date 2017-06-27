de0f6870525965f94ecbd83b13c6e3837a78cced
Michael Diglio <mdiggss@gmail.com>
2017-06-27 10:17:59 -0400

    Javascript Updates
    Added Skill Section drop-downs.
    Added disappearing navigation when scrolling down and reappear when scrolling up

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,11 +1,54 @@
 $(document).ready(function(){
+  //mobile nav hamburger controls
   $('.hamburger').click(function(){
     $(this).toggleClass('open');
     $('.mobile-nav').slideToggle();
   });
 
-  $('body').delay(9000).queue(function(){
-    $(this).css("overflow", "visible");
-    $('.loading-overlay').remove();
+  $('.iconRed').click(function(){
+    $('.setup').slideToggle();
+  });
+  $('.iconOrange').click(function(){
+    $('.bikingGear').slideToggle();
+  });
+  $('.iconPurple').click(function(){
+    $('.readingList').slideToggle();
+  });
+  $('.iconBlue').click(function(){
+    $('.coffeeFreak').slideToggle();
+  });
+  $('.iconGreen').click(function(){
+    $('.hiking').slideToggle();
+  });
+
+  // $('body').delay(9000).queue(function(){
+  //   $(this).css("overflow", "visible");
+  //   $('.loading-overlay').remove();
+  // });
+
+  //Header appearance when scrolling
+  $(window).scroll(
+    {
+        previousTop: 0
+    },
+    function () {
+    var currentTop = $(window).scrollTop();
+    var headerHeight = $("header").outerHeight();
+    if($(window).scrollTop() > headerHeight){
+    if (currentTop <= this.previousTop) {
+        $("header").css("opacity", "1");
+        $("header").css("pointer-events", "auto");
+        $(".mobile-nav").css("opacity", "1");
+        $(".mobile-nav").css("pointer-events", "auto");
+
+    } else {
+       $("header").css("opacity", "0");
+       $("header").css("pointer-events", "none");
+       $(".mobile-nav").css("opacity", "0");
+       $(".mobile-nav").css("pointer-events", "none");
+
+    }
+  }
+    this.previousTop = currentTop;
   });
 });
