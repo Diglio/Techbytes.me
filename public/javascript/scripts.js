d4741748fc2bea9bd3e29dc8696babcec6c16a7c
Michael Diglio <mdiggss@gmail.com>
2017-07-01 16:23:43 -0400

    Updated slider for Skills section

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,11 +1,118 @@
 $(document).ready(function(){
+  //mobile nav hamburger controls
   $('.hamburger').click(function(){
     $(this).toggleClass('open');
     $('.mobile-nav').slideToggle();
   });
 
-  $('body').delay(9000).queue(function(){
-    $(this).css("overflow", "visible");
-    $('.loading-overlay').remove();
+  $('.iconRed').click(function(){
+    if($('.bikingGear').css('display') === 'block'){
+      $('.bikingGear').slideToggle();
+    }
+    if($('.readingList').css('display') === 'block'){
+      $('.readingList').slideToggle();
+    }
+    if($('.coffeeFreak').css('display') === 'block'){
+      $('.coffeeFreak').slideToggle();
+    }
+    if($('.hiking').css('display') === 'block'){
+      $('.hiking').slideToggle();
+    }
+    $('.setup').slideToggle();
+  });
+
+  $('.iconOrange').click(function(){
+    if($('.setup').css('display') === 'block'){
+      $('.setup').slideToggle();
+    }
+    if($('.readingList').css('display') === 'block'){
+      $('.readingList').slideToggle();
+    }
+    if($('.coffeeFreak').css('display') === 'block'){
+      $('.coffeeFreak').slideToggle();
+    }
+    if($('.hiking').css('display') === 'block'){
+      $('.hiking').slideToggle();
+    }
+    $('.bikingGear').slideToggle();
+  });
+
+  $('.iconPurple').click(function(){
+    if($('.bikingGear').css('display') === 'block'){
+      $('.bikingGear').slideToggle();
+    }
+    if($('.setup').css('display') === 'block'){
+      $('.setup').slideToggle();
+    }
+    if($('.coffeeFreak').css('display') === 'block'){
+      $('.coffeeFreak').slideToggle();
+    }
+    if($('.hiking').css('display') === 'block'){
+      $('.hiking').slideToggle();
+    }
+    $('.readingList').slideToggle();
+  });
+
+  $('.iconBlue').click(function(){
+    if($('.bikingGear').css('display') === 'block'){
+      $('.bikingGear').slideToggle();
+    }
+    if($('.readingList').css('display') === 'block'){
+      $('.readingList').slideToggle();
+    }
+    if($('.setup').css('display') === 'block'){
+      $('.setup').slideToggle();
+    }
+    if($('.hiking').css('display') === 'block'){
+      $('.hiking').slideToggle();
+    }
+    $('.coffeeFreak').slideToggle();
+  });
+
+  $('.iconGreen').click(function(){
+    if($('.bikingGear').css('display') === 'block'){
+      $('.bikingGear').slideToggle();
+    }
+    if($('.readingList').css('display') === 'block'){
+      $('.readingList').slideToggle();
+    }
+    if($('.coffeeFreak').css('display') === 'block'){
+      $('.coffeeFreak').slideToggle();
+    }
+    if($('.setup').css('display') === 'block'){
+      $('.setup').slideToggle();
+    }
+    $('.hiking').slideToggle();
+  });
+
+  // $('body').delay(9000).queue(function(){
+  //   $(this).css("overflow", "visible");
+  //   $('.loading-overlay').remove();
+  // });
+
+  //Header appearance when scrolling
+  $(window).scroll(
+    {
+        previousTop: 0
+    },
+    function () {
+    var currentTop = $(window).scrollTop();
+    var headerHeight = $("header").outerHeight();
+    if($(window).scrollTop() > headerHeight){
+    if (currentTop <= this.previousTop) {
+        $("header").css("opacity", "1");
+        $("header").css("pointer-events", "auto");
+        $(".mobile-nav").css("opacity", "1");
+        $(".mobile-nav").css("pointer-events", "auto");
+
+    } else {
+       $("header").css("opacity", "0");
+       $("header").css("pointer-events", "none");
+       $(".mobile-nav").css("opacity", "0");
+       $(".mobile-nav").css("pointer-events", "none");
+
+    }
+  }
+    this.previousTop = currentTop;
   });
 });
